fb3254dbb440f99341a41dc56f121528b0496a71
orcunakkaya <berkayakkaya49@gmail.com>
2021-11-05 12:27:43 +0300

    Static files added

--- app.js
+++ app.js
@@ -2,18 +2,30 @@ const  express = require('express');
 
 const app = express();
 
-app.get("/", (req, res) => {
 
-    const blog = {
-        id: 1, 
-        title: "Blog title", 
-        description: "Blog description"
-    }
+//Middlewears
+app.use(express.static('public'))
 
-    res.send(blog);
+// Templete engine
+app.set("view engine", "ejs");
+
+app.get('/', (req, res) => {
+    res.render('index')
 })
 
-const port = 4000;
+app.get('/about', (req, res) => {
+    res.render('about')
+})
+
+app.get('/add_post', (req, res) => {
+    res.render('add_post')
+})
+
+app.get('/post', (req, res) => {
+    res.render('post')
+})
+
+const port = 5000;
 
 app.listen(port, () => {
     console.log(`Sunucu ${port} portunda başlatıldı...'`);
